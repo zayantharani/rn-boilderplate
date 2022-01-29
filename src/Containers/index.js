caa1c4c53942c99470c9aa1a3a0bdc0515310f86
Zayan Tharani <zayantharani@folio3.com>
2022-01-29 22:42:23 +0500

    Initial Commit

---
+++ src/Containers/index.js
@@ -0,0 +1,2 @@
+export { default as SignInContainer } from './SignInContainer'
+export { default as StartupContainer } from './StartupContainer'
---
+++ src/Translations/resources/en.js
@@ -0,0 +1,12 @@
+export default {
+  welcome: 'AKSWBP',
+  actions: {
+    continue: 'Continue',
+  },
+  signIn: {
+    desc: 'Enter your user id and password to proceed',
+    labels: {
+      userId: 'Enter a user id',
+    },
+  },
+}
